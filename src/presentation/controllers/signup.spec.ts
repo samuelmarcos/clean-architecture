25c109453d4bf2c9b4fe2786b163ecddfef73d8a
samuelmarcos <samuelmarcos9580@gmail.com>
2021-04-21 13:40:38 -0300

    feat: should return 400 if no password or password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,34 @@ describe('Signup Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
+
+    test('should return 400 if no name is provided', () => {
+        const sut = new SignUpController()
+
+        const httpRequest = {
+            body: {
+                name: 'any_email',
+                email: 'any_email',
+                password_confirmation: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    })
+
+    test('should return 400 if no name is provided', () => {
+        const sut = new SignUpController()
+
+        const httpRequest = {
+            body: {
+                name: 'any_email',
+                email: 'any_email',
+                password: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password_confirmation'))
+    })
 })
