8e718ce4e2566b5d5cc681ca5b8ed1b70171be95
samuelmarcos <samuelmarcos9580@gmail.com>
2021-07-29 21:48:34 -0300

    refactor: mongo helper

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo respository', () => {
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    test('should return an account on success', async () => {
+        const sut = new AccountMongoRepository()
+
+        const account = await sut.add({
+            name: "any_name",
+            email: "any_email@email.com.br",
+            password: "any_password",
+        })
+
+        expect(account).toBeTruthy()
+        expect(account.name).toBe("any_name")
+        expect(account.email).toBe("any_email@email.com.br")
+        expect(account.password).toBe("any_password")
+    })
+})
