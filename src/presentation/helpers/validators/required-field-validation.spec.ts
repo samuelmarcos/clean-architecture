fabfec29de22e8fc1cb1327e93fc589b8d5ac950
samuelmarcos <samuelmarcos9580@gmail.com>
2021-08-24 17:42:21 -0300

    refactor: move sut creation to separate method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,10 +1,20 @@
 import { MissingParamError } from "../../errors"
 import { RequiredFieldValidation } from "./required-field-validation"
 
+const makeSut = () => {
+    return new RequiredFieldValidation('field')
+}
+
 describe('Required Field Validation', () => {
-    test('should return a minssing param error if validation fails', () => {
-        const sut = new RequiredFieldValidation('field')
+    test('should return a missing param error if validation fails', () => {
+        const sut = makeSut()
         const error = sut.validate({ name: 'any_name' })
         expect(error).toEqual(new MissingParamError('field'))
+    })
+
+    test('should not return if validation success', () => {
+        const sut = makeSut()
+        const error = sut.validate({ field: 'any_name' })
+        expect(error).toBeFalsy()
     })
 })
