95480b8224071772b474c98f2fed22482585faff
samuelmarcos <samuelmarcos9580@gmail.com>
2021-08-24 18:04:02 -0300

    refactor: move sut to a refactor method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,11 +2,24 @@ import { MissingParamError } from "../../errors"
 import { Validation } from "./validation"
 import { ValidationComposite } from "./validation-composite"
 
+interface SutTypes {
+    sut: ValidationComposite
+    validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+    const validationStub = makeValidationStub()
+    const sut = new ValidationComposite([ validationStub ])
+    return {
+        sut,
+        validationStub
+    }
+}
 
 const makeValidationStub = (): Validation => {
     class ValidationStub implements Validation {
-        validate(input: string): Error {
-             return new MissingParamError('field')
+        validate(input: string): any {
+             return null
         }
     }
     return new ValidationStub()
@@ -15,7 +28,8 @@ const makeValidationStub = (): Validation => {
 
 describe('Validation Composite', () => {
     test('should return an error if any validation fails', () => {
-        const sut = new ValidationComposite([ makeValidationStub() ])
+        const { sut, validationStub } = makeSut()
+        jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
         const error = sut.validate({ field: 'any_value' })
         expect(error).toEqual(new MissingParamError('field'))
     })
