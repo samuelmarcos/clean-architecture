2ff59c370bb6eda891aa9075a918d4b6942c2851
samuelmarcos <samuelmarcos9580@gmail.com>
2021-11-29 22:35:59 -0300

    feat: ensure bcrypt adpter calls bcrypt compare with correct values

--- src/infra/cryptography/bcrypt-adpter.spec.ts
+++ src/infra/cryptography/bcrypt-adpter.spec.ts
@@ -4,25 +4,37 @@ import { BcryptAdapter } from './bcrypt-adpter'
 jest.mock('bcrypt', () => ({
     async hash(): Promise<string> {
         return new Promise(resolve => resolve('hash'))
+    },
+
+    async compare(): Promise<boolean> {
+        return new Promise(resolve => resolve(true))
     }
 }))
+
 const salt = 12
 const makeSut = () => {
     return  new BcryptAdapter(salt)
 }
 
 describe('Bcrypt Adapter', () => {
-    test('should call bcrypt with correct value', async () => {
+    test('should call hash with correct value', async () => {
         const sut  = makeSut()
         const hashSpy = jest.spyOn(bcrypt, 'hash')
-        await sut.encrypt('any_value')
+        await sut.hash('any_value')
         expect(hashSpy).toHaveBeenLastCalledWith('any_value', salt)
     })
 
-    test('should return a hash on success', async () => {
+    test('should return a valid hash on hash success', async () => {
         const sut  = makeSut()
     
-        const hashedValue = await sut.encrypt('any_value')
+        const hashedValue = await sut.hash('any_value')
         expect(hashedValue).toBe('hash')
+    })
+
+    test('should call compare with correct value', async () => {
+        const sut  = makeSut()
+        const comparehSpy = jest.spyOn(bcrypt, 'compare')
+        await sut.compare('any_value', 'any_hash')
+        expect(comparehSpy).toHaveBeenLastCalledWith('any_value', 'any_hash')
     })
 })
--- src/infra/cryptography/bcrypt-adpter.ts
+++ src/infra/cryptography/bcrypt-adpter.ts
@@ -1,7 +1,8 @@
 import { Hasher } from '../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
+import { HashCompare } from '../../data/protocols/cryptography/hash-compre'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashCompare {
     private readonly salt: number
     constructor(salt: number) {
         this.salt = salt
@@ -9,5 +10,10 @@ export class BcryptAdapter implements Hasher {
     async hash(value: string): Promise<string> {
         const hash =  await bcrypt.hash(value, this.salt)
         return hash
+    }
+
+    async compare(value: string, hash: string): Promise<boolean> {
+        await bcrypt.compare(value, hash);
+        return Promise.resolve(true);
     }
 }
