fc6de8b281123b3c0f00a547924bf2d3fda65229
samuelmarcos <samuelmarcos9580@gmail.com>
2022-03-30 00:16:04 -0300

    refactor: refactor class

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -10,19 +10,14 @@ export class SignUpController implements Controller {
 
     public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-
             const error = this.validation.validate(httpRequest.body)
 
             if(error) return badRequest(error)
-
             const { name, email, password } = httpRequest.body;
-            
             const account = await this.addAccount.add({ name, email, password})
             
             await this.authentication.auth({ email, password})
-
             return ok(account)
-
         } catch (error) {
             return serverError(error)
         }
