68a457dee2cbc3c4b329dc594a29a6d0b604e43c
samuelmarcos <samuelmarcos9580@gmail.com>
2022-06-22 00:04:09 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

--- src/main/factories/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys.ts
@@ -2,9 +2,10 @@ import { AddSurvey } from '../../../../../domain/usecases/add-survey'
 import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
 import { DbAddSurvey } from '../../../../../data/usecases/add-survey/db-add-survey'
 import { LoadSurveys } from '../../../../../domain/usecases/load-surveys'
+import { DbLoadSurveys } from '../../../../../data/usecases/load-surveys/db-load-surveys'
 
 export const makeDbLoadSurveys = (): LoadSurveys => {
     const surveyMongoRepository = new SurveyMongoRepository()
-    return new DbAddSurvey(surveyMongoRepository)
+    return new DbLoadSurveys(surveyMongoRepository)
 
 } 
