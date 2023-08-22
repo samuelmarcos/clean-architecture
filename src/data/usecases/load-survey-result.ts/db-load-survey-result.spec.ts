d56f5b1a6645641c658ae9e799949a85d4cdc66d
samuelmarcos <samuelmarcos9580@gmail.com>
2023-08-21 22:34:07 -0300

    refactor: move sut to a factory mothod and the repository to a mock folder

--- src/data/usecases/load-survey-result.ts/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result.ts/db-load-survey-result.spec.ts
@@ -1,19 +1,14 @@
-import { mockSurveyResult } from '@/mocks/domain'
-import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-protocols'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+import { loadSurveyResultRepositoryMock } from '@/mocks/data/db'
 
-const loadSurveyResultRepositoryMock = () => {
-  class LoadSurveyResultRepositoryMock implements LoadSurveyResultRepository {
-    public async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
-      return Promise.resolve(mockSurveyResult())
-    }
-  }
-
-  return new LoadSurveyResultRepositoryMock()
+type SutTypes = {
+  sut: LoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
 }
 
-const makeSut = () => {
+const makeSut = (): SutTypes => {
   const loadSurveyResultRepositoryStub = loadSurveyResultRepositoryMock()
   const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
   
