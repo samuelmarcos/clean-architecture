6538f23b68676a7e7bf051077af686765ce93503
samuelmarcos <samuelmarcos9580@gmail.com>
2023-08-21 22:40:54 -0300

    refactor: move load survey result dependencie to a protocol file

--- src/data/usecases/load-survey-result.ts/db-load-survey-result.ts
+++ src/data/usecases/load-survey-result.ts/db-load-survey-result.ts
@@ -1,6 +1,4 @@
-import { LoadSurveyResult } from "@/domain/usecases/survey-result/load-survey-result";
-import { SurveyResultModel } from "../save-survey-result/db-save-survey-result-protocols";
-import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result/load-survey-result-repository";
+import { LoadSurveyResultRepository, LoadSurveyResult, SurveyResultModel } from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
 
