b101b25b4c54c17581f9643c5806a075fb92536d
samuelmarcos <samuelmarcos9580@gmail.com>
2023-08-21 22:40:59 -0300

    refactor: move load survey result dependencie to a protocol file

--- src/data/usecases/load-survey-result.ts/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result.ts/db-load-survey-result.spec.ts
@@ -1,5 +1,5 @@
 import { DbLoadSurveyResult } from './db-load-survey-result'
-import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
 import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 import { loadSurveyResultRepositoryMock } from '@/mocks/data/db'
 
