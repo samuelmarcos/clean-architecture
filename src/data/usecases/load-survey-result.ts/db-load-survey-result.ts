7c72d50c406302a09fe6f08b0caabd177484f00b
samuelmarcos <samuelmarcos9580@gmail.com>
2023-08-23 23:17:18 -0300

    refactor: repository correct name

--- src/data/usecases/load-survey-result.ts/db-load-survey-result.ts
+++ src/data/usecases/load-survey-result.ts/db-load-survey-result.ts
@@ -2,11 +2,11 @@ import { LoadSurveyResultRepository, LoadSurveyResult, SurveyResultModel, LoadSu
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
 
-  constructor(private readonly loadSurveyResult: LoadSurveyResultRepository,
+  constructor(private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
               private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
 
   public async load(surveyId: string): Promise<SurveyResultModel> {
-    const surveyResult = await this.loadSurveyResult.loadBySurveyId(surveyId)
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
     if(surveyResult !== null) {
       await this.loadSurveyByIdRepository.loadById(surveyId)
     }
